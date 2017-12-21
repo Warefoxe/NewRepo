528063ebc2ada0abd32e600745f49a2fe576ba12
Юрий Давидюк <jurassic151@gmail.com>
2017-12-21 20:40:29 +0200

    UKR The program throws two cubes until two six. How many attempts did the program do?

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
-#include <conio.h>
+// ENG
+//The program throws two cubes until two six.How many attempts did the program do ?
+// UKR
+//Програма кидає два кубики, доки  не випаде дві шістки.Скільки спроб зробила програма ?
 
-#include <random>
+#include<iostream>
 #include <time.h>
 
 using namespace std;
@@ -9,27 +11,25 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int a = 1;
-	int b = 1;
-
+	srand(time(0));
+	int cube1 = 0;
+	int cube2 = 0;
+	int c = 0;
 	{
 
-		for (int c(0); c <= 5; c++) {
+		for (; !((cube1 == 6) && (cube2 == 6)); )
+		{
+			++c;
+			cube1 = 1 + rand() % 6;
+			cube2 = 1 + rand() % 6;
 
-			system("cls");
-			srand(time(NULL));
-			a = rand() % 6;
-			cout << "1 игроку выпало : " << a << endl;
-			b = rand() % 6;
-			cout << "1 игроку выпало : " << b << endl;
-
-			if (a > b) {
-				cout << "выграл 1 игрок, ему выпало: " << a << " игроку 2 выпало : " << b << endl;
-			}
-			else { cout << "выграл 2 игрок, ему выпало: " << a << " игроку 2 выпало : " << b << endl; }
-			system("pause");
-
-
-			return 0;
+			cout << "#" << c << " спроба :" << " Куб#1 = " << cube1 << " i" << " Куб#2 = " << cube2 << endl;
 		}
+
+
+		cout << "Випало дві шістки на спробi #" << c << endl;
+		system("pause");
+
+		return 0;
 	}
+}
